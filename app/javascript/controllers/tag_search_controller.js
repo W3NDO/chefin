f2a947510de0e12af8694c19fb19d033dc9d8d9f
W3NDO <wendonyang@gmail.com>
2026-01-06 14:52:06 +0300

    feat(develop/recipes) - Added stimulus controllers to the discover page that shows and hides tags on search

--- app/javascript/controllers/tag_search_controller.js
+++ app/javascript/controllers/tag_search_controller.js
@@ -1,12 +1,29 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static debounces = ['getTags']
+  static targets = [ 'tags' ]
+
   connect() {
+    window.StimulusUse.useDebounce(this, { wait: 1000 })
   }
 
-  static targets = [ "query" ]
+  getTags(event) {
+    let search_term = event.target.value.toLowerCase()
+    let tag_ids = Object.fromEntries(
+    this.tagsTargets.map((target) => [
+        target.id,                    
+        target.id.replaceAll("-", " ")
+      ])
+    );
 
-  getTags() {
-    console.log(`${this.queryTarget}`)
+    Object.keys(tag_ids).map( (key) => {
+      let existing_classlist = Array.from(document.getElementById(key).classList)
+      if (!tag_ids[key].includes(search_term)){
+        document.getElementById(key).classList.add("hidden")
+      } else {
+        document.getElementById(key).classList.remove("hidden")
+      }
+    })
   }
 }
